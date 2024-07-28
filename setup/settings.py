864d1fcc29a688c570466a86e398d39d317256a9
Dev-Znot <thalles.zainote11@gmail.com>
2024-07-28 16:34:31 -0300

    feat: config/db

--- setup/settings.py
+++ setup/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
     }
 }
 
